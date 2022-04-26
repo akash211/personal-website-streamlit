b4b282ad35c244f21ebf5c3a9ecb1278edbaa20d
Akash Kumar <akash211@gmail.com>
2022-04-26 23:03:46 +0530

    converted to only functions module

--- ConsoleToDatabase.py
+++ ConsoleToDatabase.py
@@ -3,6 +3,9 @@ import re
 from datetime import datetime as dt
 import time
 import os
+
+import numpy as np
+from selenium.common.exceptions import TimeoutException
 from sqlalchemy import create_engine
 import urllib
 import pyodbc
@@ -15,8 +18,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-
-wait = 10
 
 
 # Creating chrome function
@@ -81,18 +82,18 @@ def breakup_dividend_data(bot_, i_):
 
     # Getting breakup and dividend details
     a.move_to_element(t).perform()
-    bot_.find_elements(By.CLASS_NAME, 'flag-container')[i_].click()
-    WebDriverWait(bot_, wait).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[contains(text(), "breakdown")]'))
-    )
+    bot_.find_elements(By.CLASS_NAME, 'context-menu-button')[i_].click()
+    # WebDriverWait(bot_, wait_).until(
+    #     EC.element_to_be_clickable((By.XPATH, '//*[contains(text(), "breakdown")]'))
+    # )
     bot_.find_element(By.XPATH, '//*[contains(text(), "breakdown")]').click()
     delay()
     breakdown = pd.read_html(bot_.page_source)[1]
     bot_.find_element(By.CLASS_NAME, 'close-modal').click()
     delay()
     a.move_to_element(t).perform()
-    bot_.find_elements(By.CLASS_NAME, 'flag-container')[i_].click()
-    WebDriverWait(bot_, wait).until(
+    bot_.find_elements(By.CLASS_NAME, 'context-menu-button')[i_].click()
+    WebDriverWait(bot_, wait_).until(
         EC.element_to_be_clickable((By.XPATH, '//*[contains(text(), "dividend")]'))
     )
     bot_.find_element(By.XPATH, '//*[contains(text(), "dividend")]').click()
@@ -100,7 +101,7 @@ def breakup_dividend_data(bot_, i_):
     try:
         dividend_ = pd.read_html(bot_.page_source)[1]
     except IndexError:
-        dividend_ = 0
+        dividend_ = pd.DataFrame(columns=['Date', 'Qty', 'Dividend / Share (DPS)', 'Total dividend'])
     bot_.find_element(By.CLASS_NAME, 'close-modal').click()
     return [breakdown, dividend_]
 
@@ -113,128 +114,142 @@ def write_table(df, tbl_name, if_exists='append'):
     df.to_sql(tbl_name, schema='dbo', con=engine, index=False, if_exists=if_exists)
 
 
-# Creating chrome instance and login to console and opening Holdings section
-bot = console_login(chrome(), os.environ['zerodha_username'], os.environ['zerodha_password'], os.environ['zerodha_pin'])
-
-# Last Updated date
-last_update_date = re.findall(r'(\d\d\d\d-\d\d-\d\d)', bot.find_element(By.CLASS_NAME, 'last-updated').text)[0]
-
-# Getting portfolio details table
-portfolio_table = getting_stocks(bot)
-
-portfolio_table['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
-portfolio_table['source_last_updated'] = last_update_date
-
-write_table(portfolio_table.drop('Prev. close', axis=1), 'TargetTable', if_exists='replace')
+def replace_function(date_with_attributes):
+    return date_with_attributes.replace(' ipo', '').replace(' bonus', '')
 
 
-# Getting breakup and dividend table details
-breakup, dividend = breakup_dividend_data(bot, 9)
-print("~~~~~~~~~~~")
-print(breakup)
-print("~~~~~~~~~~~")
-print(dividend)
-print("~~~~~~~~~~~")
+def sector(name='current'):
+    sector_allocation = bot.find_element(By.CLASS_NAME, 'scLabels').text
+    sector_name, allocation_percentage = [], []
 
-breakup_details = 0
+    for index, _ in enumerate(sector_allocation.split("\n")):
+        if index % 2 != 0:
+            allocation_percentage.append(_)
+        else:
+            sector_name.append(_)
+    return pd.DataFrame({'sector_name': sector_name, 'allocation_percentage_' + name: allocation_percentage})
 
-for stock_counter in range(len(portfolio_table)):
-    breakup_dividend_data(bot, stock_counter + 1)
 
-# ****** Getting Current reports ******
-# Getting sector wise
-sector_allocation = bot.find_element(By.CLASS_NAME, 'scLabels').text
-sector_name, allocation_percentage_now = [], []
+def cap(name='current'):
+    cap_allocation = bot.find_element(By.ID, 'stock_labels').text
+    cap_name, allocation_percentage = [], []
 
-for i, _ in enumerate(sector_allocation.split("\n")):
-    if i % 2 != 0:
-        allocation_percentage_now.append(_)
-    else:
-        sector_name.append(_)
+    for i, _ in enumerate(cap_allocation.split("\n")):
+        if i % 2 != 0:
+            allocation_percentage.append(_)
+        else:
+            cap_name.append(_)
 
-sector_wise_data = pd.DataFrame({'sector_name': sector_name, 'allocation_percentage_now': allocation_percentage_now})
+    return pd.DataFrame({'cap_name': cap_name, 'allocation_percentage_' + name: allocation_percentage}).iloc[:3]
 
-# Getting cap wise 
-cap_allocation = bot.find_element(By.ID, 'stock_labels').text
-cap_name, allocation_percentage_now = [], []
 
-for i, _ in enumerate(cap_allocation.split("\n")):
-    if i % 2 != 0:
-        allocation_percentage_now.append(_)
-    else:
-        cap_name.append(_)
+def breakup_dividend_complete():
+    # Getting breakup and dividend table details
+    breakup_details = pd.DataFrame(columns=['Symbol', 'Date', 'Qty.', 'Price', 'Age (days)', 'P&L'])
+    dividend_details = pd.DataFrame(columns=['Symbol', 'Date', 'Qty', 'Dividend / Share (DPS)', 'Total dividend'])
+    for stock_counter in range(len(portfolio_table)):
+        symbol = portfolio_table.iloc[stock_counter]['Symbol']
+        print(symbol)
+        try:
+            breakup, dividend = breakup_dividend_data(bot, stock_counter)
+            breakup = breakup[np.arange(len(breakup)) % 2 == 0]
+            breakup['Symbol'] = symbol
+            breakup_details = pd.concat([breakup_details, breakup])
+            if len(dividend) != 0:
+                dividend['Symbol'] = symbol
+                dividend_details = pd.concat([dividend_details, dividend])
+        except TimeoutException:
+            print(f"Error for {symbol}")
+    breakup_details['Date'] = breakup_details['Date'].apply(replace_function)
+    breakup_details['source_last_updated'] = last_update_date
+    breakup_details['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
+    dividend_details['source_last_updated'] = last_update_date
+    dividend_details['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
+    write_table(dividend_details, 'dividend', if_exists='replace')
+    write_table(breakup_details, 'breakup', if_exists='replace')
 
-stocks_wise_data = pd.DataFrame({'cap_name': cap_name, 'allocation_percentage_now': allocation_percentage_now})
-cap_wise_data = stocks_wise_data.iloc[:3]
 
-# ****** Getting Invested Reports *******
-bot.find_elements(By.CLASS_NAME, 'su-radio-label')[1].click()
+def sector_cap_complete():
+    sector_wise_data = sector()
+    cap_wise_data = cap()
+    bot.find_elements(By.CLASS_NAME, 'su-radio-label')[1].click()
+    delay()
+    sector_wise_data_ = sector(name='invested')
+    cap_wise_data_ = cap(name='invested')
+    cap_wise_data = pd.merge(cap_wise_data, cap_wise_data_, on="cap_name")
+    sector_wise_data = pd.merge(sector_wise_data, sector_wise_data_, on="sector_name")
+    cap_wise_data['source_last_updated'] = last_update_date
+    cap_wise_data['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
+    sector_wise_data['source_last_updated'] = last_update_date
+    sector_wise_data['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
+    write_table(cap_wise_data, 'cap_table', if_exists='replace')
+    write_table(sector_wise_data, 'sector_table', if_exists='replace')
 
-# Getting sector wise
-sector_allocation = bot.find_element(By.CLASS_NAME, 'scLabels').text
-sector_name, allocation_percentage_invested = [], []
 
-for i, _ in enumerate(sector_allocation.split("\n")):
-    if i % 2 != 0:
-        allocation_percentage_invested.append(_)
-    else:
-        sector_name.append(_)
+def insights_extract():
+    bot.find_element(By.CLASS_NAME, 'insights').click()
+    bot.find_element(By.CLASS_NAME, 'su-checkbox-label').click()
+    iframe = bot.find_element(By.ID, 'insights_frame')
+    WebDriverWait(bot, 2 * wait).until(
+        EC.presence_of_element_located((By.ID, 'insights_frame'))
+    )
+    bot.switch_to.frame(iframe)
+    delay()
+    tickertape_elements = bot.find_elements(By.CLASS_NAME, 'card-title')
+    beta = tickertape_elements[0].text.replace("Beta (β): ", "")
+    pe_ratio = tickertape_elements[1].text.replace("PE Ratio: ", "")
+    red_flags = tickertape_elements[3].text.replace("Redflags: ", "").replace("🚩", "")
+    overall_price_forecast = tickertape_elements[2].text.replace("Price Forecast: ", "")
+    insights = pd.DataFrame(
+        {'Beta': beta, 'PE_ratio': pe_ratio, 'overall_price_forecast': overall_price_forecast, 'Red_Flags': red_flags,
+         'source_last_updated': last_update_date,
+         'last_updated': dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")}, index=[0])
+    write_table(insights, 'insights', if_exists='replace')
 
-sector_wise_data_ = pd.DataFrame(
-    {'sector_name': sector_name, 'allocation_percentage_invested': allocation_percentage_invested})
 
-# Getting cap wise
-cap_allocation = bot.find_element(By.ID, 'stock_labels').text
-cap_name, allocation_percentage_invested = [], []
+def forecast_function():
+    bot.find_element(By.CLASS_NAME, 'card-link').click()
+    portfolio_forecast_table_details = bot.find_element(By.CLASS_NAME, 'insight-container').text
+    bot.find_element(By.CLASS_NAME, 'nav-arrow-button').click()
+    delay()
+    portfolio_forecast_table_details += bot.find_element(By.CLASS_NAME, 'insight-container').text
+    bot.find_elements(By.CLASS_NAME, 'nav-arrow-button')[1].click()
+    delay()
+    portfolio_forecast_table_details += bot.find_element(By.CLASS_NAME, 'insight-container').text
+    bot.find_elements(By.CLASS_NAME, 'nav-arrow-button')[1].click()
+    delay()
+    portfolio_forecast_table_details += bot.find_element(By.CLASS_NAME, 'insight-container').text
+    regex_pattern_for_getting_forecast = re.compile(r"([A-Z]+) ([\d.%]+)\n.+?([\d.%]+).+?([\d.]+%)")
+    forecast_list = regex_pattern_for_getting_forecast.findall(portfolio_forecast_table_details)
+    forecast_dataframe = pd.DataFrame(forecast_list,
+                                      columns=['stock_name', 'forecast%', 'expected_return%', 'last3years_cagr%'])
+    forecast_dataframe['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
+    forecast_dataframe['source_last_updated'] = last_update_date
+    write_table(forecast_dataframe, 'forecast', if_exists='replace')
 
-for i, _ in enumerate(cap_allocation.split("\n")):
-    if i % 2 != 0:
-        allocation_percentage_invested.append(_)
-    else:
-        cap_name.append(_)
 
-stocks_wise_data = pd.DataFrame(
-    {'cap_name': cap_name, 'allocation_percentage_invested': allocation_percentage_invested})
-cap_wise_data_ = stocks_wise_data.iloc[:3]
+def main():
+    global wait, wait_, bot, last_update_date, portfolio_table
+    wait = 10
+    wait_ = 2
+    # Creating chrome instance and login to console and opening Holdings section
+    bot = console_login(chrome(), os.environ['zerodha_username'], os.environ['zerodha_password'],
+                        os.environ['zerodha_pin'])
+    # Last Updated date
+    last_update_date = re.findall(r'(\d\d\d\d-\d\d-\d\d)', bot.find_element(By.CLASS_NAME, 'last-updated').text)[0]
+    # Getting portfolio details table
+    portfolio_table = getting_stocks(bot)
+    portfolio_table['last_update_date'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M:%S")
+    portfolio_table['source_last_updated'] = last_update_date
+    write_table(portfolio_table.drop('Prev. close', axis=1), 'TargetTable', if_exists='replace')
+    breakup_dividend_complete()
+    # Getting sector and cap wise data
+    sector_cap_complete()
+    # Getting Insights
+    insights_extract()
+    # PriceForecast details
+    forecast_function()
 
-# Merging the current and invested datasets
-cap_wise_data = pd.merge(cap_wise_data, cap_wise_data_, on="cap_name")
-sector_wise_data = pd.merge(sector_wise_data, sector_wise_data_, on="sector_name")
 
-# Getting Insights
-bot.find_element(By.CLASS_NAME, 'insights').click()
-bot.find_element(By.CLASS_NAME, 'su-checkbox-label').click()
-
-iframe = bot.find_element(By.ID, 'insights_frame')
-
-WebDriverWait(bot, 2 * wait).until(
-    EC.presence_of_element_located((By.ID, 'insights_frame'))
-)
-
-bot.switch_to.frame(iframe)
-
-tickertape_elements = bot.find_elements(By.CLASS_NAME, 'card-title')
-Beta = tickertape_elements[0].text.replace("Beta (β): ", "")
-PE_ratio = tickertape_elements[1].text.replace("PE Ratio: ", "")
-Red_Flags = tickertape_elements[3].text.replace("Redflags: ", "").replace("🚩", "")
-
-# PriceForecast details
-overall_price_forecast = tickertape_elements[2].text.replace("Price Forecast: ", "")
-bot.find_element(By.CLASS_NAME, 'card-link').click()
-portfolio_forecast_table_details = bot.find_element(By.CLASS_NAME, 'insight-container').text
-bot.find_element(By.CLASS_NAME, 'nav-arrow-button').click()
-delay()
-portfolio_forecast_table_details += bot.find_element(By.CLASS_NAME, 'insight-container').text
-bot.find_elements(By.CLASS_NAME, 'nav-arrow-button')[1].click()
-delay()
-portfolio_forecast_table_details += bot.find_element(By.CLASS_NAME, 'insight-container').text
-bot.find_elements(By.CLASS_NAME, 'nav-arrow-button')[1].click()
-delay()
-portfolio_forecast_table_details += bot.find_element(By.CLASS_NAME, 'insight-container').text
-
-regex_pattern_for_getting_forecast = re.compile(r"([A-Z]+) ([\d.%]+)\n.+?([\d.%]+).+?([\d.]+%)")
-
-forecast_list = regex_pattern_for_getting_forecast.findall(portfolio_forecast_table_details)
-
-forecast_dataframe = pd.DataFrame(forecast_list,
-                                  columns=['stock_name', 'forecast%', 'expected_return%', 'last3years_cagr%'])
+if __name__ == '__main__':
+    main()
--- database.py
+++ database.py
@@ -1,4 +1,26 @@
+from ConsoleToDatabase import *
 
+a = ActionChains(bot)
+
+t = bot.find_elements(By.CLASS_NAME, 'textleft')[3]
+
+# Getting breakup and dividend details
+a.move_to_element(t).perform()
+bot.find_elements(By.CLASS_NAME, 'context-menu-button')[3].click()
+
+bot.find_element(By.XPATH, '//*[contains(text(), "breakdown")]').click()
+delay()
+breakdown = pd.read_html(bot.page_source)[1]
+bot.find_element(By.CLASS_NAME, 'close-modal').click()
+delay()
+a.move_to_element(t).perform()
+bot.find_elements(By.CLASS_NAME, 'context-menu-button')[3].click()
+WebDriverWait(bot, wait_).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[contains(text(), "dividend")]'))
+)
+bot.find_element(By.XPATH, '//*[contains(text(), "dividend")]').click()
+delay()
+dividend_ = pd.read_html(bot.page_source)[1]
 
 
 
